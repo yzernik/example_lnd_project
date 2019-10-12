63685e1741be2bf03362908add515f2cfa96e730
Jonathan Zernik <yzernik@gmail.com>
2019-10-12 15:32:38 -0700

    Initial commit

---
+++ docker/app/start-app.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# exit from script if error was raised.
+set -e
+
+exec python -m src.app
---
+++ example_lnd_project/app.py
@@ -0,0 +1,11 @@
+from .rpc_lightning_client import RPCLightningClient
+
+
+def main():
+  print("Hello World!")
+  client = RPCLightningClient()
+  print(client.get_wallet_balance())
+
+
+if __name__== "__main__":
+  main()
