2ab085983a96d47669969c922e09dc6a5384e05b
Jonathan Zernik <yzernik@gmail.com>
2019-10-12 17:06:31 -0700

    Remove hello world lin.e

--- example_lnd_project/app.py
+++ example_lnd_project/app.py
@@ -2,7 +2,6 @@ from .rpc_lightning_client import RPCLightningClient
 
 
 def main():
-  print("Hello World!")
   client = RPCLightningClient()
   print("Wallet balance:")
   print(client.get_wallet_balance())
